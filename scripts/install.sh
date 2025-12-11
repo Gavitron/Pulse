f93eb4ace891488e9fedec823da78ecd76053ad4
Gavin McDonald <gmcdonald@sonatype.com>
2025-12-10 23:57:59 -0800

    removed trailing whitespace

--- scripts/install.sh
+++ scripts/install.sh
@@ -32,7 +32,7 @@ trap cleanup EXIT
 
 # --- Check Root ---
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root. Please use sudo." 
+   echo "This script must be run as root. Please use sudo."
    exit 1
 fi
 
